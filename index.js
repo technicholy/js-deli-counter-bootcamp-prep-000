2147fd6958927ac4f54cc8c7e2693938f733b45f
Clayton Pierce <bamer78@gmail.com>
2020-08-21 20:24:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function currentLine(waitingArray){
   if (waitingArray.length === 0){
     return "The line is currently empty."
   }else{
-    var retStr = 'The line is currently:'
+    var retStr = "The line is currently:"
     for (let i = 0; i < waitingArray.length; i++){
-      retStr.concat(`${i}`. ${waitingArray[i]})
+      retStr.concat(`${i}. ${waitingArray[i]}`)
     }
     return retStr
   }
